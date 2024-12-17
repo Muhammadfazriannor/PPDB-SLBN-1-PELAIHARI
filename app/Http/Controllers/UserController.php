8fed81cea1e91325ac7fa3352a4687f218746104
MRD_EXE <68524961+MRD127@users.noreply.github.com>
2024-12-17 16:05:02 +0800

    update user controler nambah halaman baru

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    // Menampilkan halaman dashboard pengguna
     public function index()
     {
         return view('user.dashboard');
     }
-}
 
+    // Menampilkan halaman layanan pengguna
+    public function tentang()
+    {
+        return view('user.tentang');
+    }
+}
